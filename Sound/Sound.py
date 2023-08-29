32ab5aa2f0a9792428847c493a51b602b9a26e07
P N <noah@pencherek.ch>
2023-08-29 08:49:16 +0200

    28.08.2023

--- Sound/Sound.py
+++ Sound/Sound.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pydub import AudioSegment
 from scipy.fft import rfft, rfftfreq
-import matplotlib.pyplot as plt
+from scipy.signal import find_peaks_cwt
 
 class Sound:
     def __init__(self, path: str):
@@ -13,13 +13,34 @@ class Sound:
         self.nSampleNormalised = int(self.SAMPLE_RATE * self.DURATION)
         self.yf = rfft(np.array(self.monoSound.get_array_of_samples()))
         self.xf = rfftfreq(self.nSampleNormalised, 1/self.SAMPLE_RATE)
+        self.tblMIDI = np.loadtxt("SoundSample/MIDI_NOTES.txt", delimiter=';')
+        self.idxMIDI, self.freqMIDI = np.split(self.tblMIDI, 2, axis=1)
 
-    def test(self):
-        self.sample = self.monoSound[:1000]
-        self.SAMPLE_RATE = self.sample.frame_rate
-        self.DURATION = self.sample.duration_seconds
-        self.nSampleNormalised = int(self.SAMPLE_RATE * self.DURATION)
-        self.yf = rfft(np.array(self.sample.get_array_of_samples()))
-        self.xf = rfftfreq(self.nSampleNormalised, 1/self.SAMPLE_RATE)
+    def getChunk(self, x: int, y: int):
+        if 0 <= x < len(self.monoSound) and 0 <= y < len(self.monoSound):
+            return self.monoSound[x:y]
+        else:
+            print("Warning : Bound outside scop")
+            return self.monoSound
+
+    def getXf(self, chunk: AudioSegment):
+        sample_rate = chunk.frame_rate
+        duration = chunk.duration_seconds
+        normal = int(sample_rate * duration)
+        return rfftfreq(normal, 1/sample_rate)
+
+    def getYf(self, chunk: AudioSegment):
+        return rfft(np.array(chunk.get_array_of_samples()))
+
+    def getPeakIndices(self, yf, width):
+        return find_peaks_cwt(yf, width)
+
+    def find_nearest(self, array, value):
+        #array = np.asarray(array)
+        idx = (np.abs(array - value)).argmin()
+        return array[idx]
+
+
+
 
 
--- __main__.py
+++ __main__.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from Sound.Sound import Sound
 
 sound = Sound("SoundSample/5.wav")
-#sound.test()
-for i in range(999):
-    print(f"x : {sound.xf[i]}\ny : {np.abs(sound.yf[i])}")
+chunk = sound.getChunk(0, 100)
+xf = sound.getXf(chunk)
+yf = np.log10(np.abs(sound.getYf(chunk)))
+peaks = sound.getPeakIndices(yf, [1, 2, 3])
+tbl = []
+for i in peaks:
+    if yf[i] > 9:
+        temp = sound.find_nearest(sound.freqMIDI, xf[i])[0]
+        tbl.append([temp, yf[i]])
+
+np.savetxt("SoundSample/dump", tbl, delimiter=',', newline='\n', encoding="utf8", fmt="%0.2f")#, fmt="%3.0f"
+
+#Get the MIDI number from frequence
+print(np.where(sound.tblMIDI == sound.find_nearest(sound.freqMIDI, 440))[0][0])
 
 
-
-
+#plt.plot(xf, yf)
+#plt.show()
